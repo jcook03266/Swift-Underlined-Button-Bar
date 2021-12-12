bd9cd40b1db3923198f2e270ea868de5f4440e2e
Justin Cook <jcook03266@gmail.com>
2021-12-12 10:43:22 -0500

    Update ScrollViewExample.swift
    Added code for constructing the stack view embedded in the scroll view for easy reference.

--- ScrollViewExample.swift
+++ ScrollViewExample.swift
@@ -1,4 +1,171 @@
-/** ScrollView Button Bar Methods*/
+   
+    private var navItem = UINavigationItem(title: "Developer Console 📱")
+    private var closeButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(closeButton))
+    private var navBar = UINavigationBar()
+    private var databaseButton = UIButton()
+    private var coreDataButton = UIButton()
+    private var metricsButton = UIButton()
+    private var adminsButton = UIButton()
+    private var buttonBar: underlinedButtonBar? = nil
+    /** The various different views will be hosted inside of this scrollview stack view combination*/
+    private var scrollView = UIScrollView()
+    private var stackView = UIStackView()
+    private var viewHasLoaded = false
+    private var databaseView = UIView()
+    private var coreDataView = UIView()
+    private var metricsView = UIView()
+    private var adminsView = UIView()
+    private var currentPage = 0
+
+/** Customize the specified buttons that control navigation*/
+    func setButtons(){
+        databaseButton.frame.size.height = 40
+        databaseButton.frame.size.width = 100
+        databaseButton.contentHorizontalAlignment = .center
+        databaseButton.setTitle("Database", for: .normal)
+        databaseButton.setTitleColor(darkModeFontColor, for: .normal)
+        databaseButton.layer.backgroundColor = UIColor.clear.cgColor
+        databaseButton.isExclusiveTouch = true
+        databaseButton.clipsToBounds = true
+        databaseButton.titleLabel?.font = getCustomFont(name: .Ubuntu_Light, size: 16)
+        databaseButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        databaseButton.addTarget(self, action: #selector(databaseButtonPressed), for: .touchDown)
+        
+        coreDataButton.frame.size.height = 40
+        coreDataButton.frame.size.width = 100
+        coreDataButton.contentHorizontalAlignment = .center
+        coreDataButton.setTitle("Core Data", for: .normal)
+        coreDataButton.setTitleColor(darkModeFontColor, for: .normal)
+        coreDataButton.layer.backgroundColor = UIColor.clear.cgColor
+        coreDataButton.isExclusiveTouch = true
+        coreDataButton.clipsToBounds = true
+        coreDataButton.titleLabel?.font = getCustomFont(name: .Ubuntu_Light, size: 16)
+        coreDataButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        coreDataButton.addTarget(self, action: #selector(coreDataButtonPressed), for: .touchDown)
+        
+        metricsButton.frame.size.height = 40
+        metricsButton.frame.size.width = 100
+        metricsButton.contentHorizontalAlignment = .center
+        metricsButton.setTitle("Metrics", for: .normal)
+        metricsButton.setTitleColor(darkModeFontColor, for: .normal)
+        metricsButton.layer.backgroundColor = UIColor.clear.cgColor
+        metricsButton.isExclusiveTouch = true
+        metricsButton.clipsToBounds = true
+        metricsButton.titleLabel?.font = getCustomFont(name: .Ubuntu_Light, size: 16)
+        metricsButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        metricsButton.addTarget(self, action: #selector(metricsButtonPressed), for: .touchDown)
+        
+        adminsButton.frame.size.height = 40
+        adminsButton.frame.size.width = 100
+        adminsButton.contentHorizontalAlignment = .center
+        adminsButton.setTitle("Admins", for: .normal)
+        adminsButton.setTitleColor(darkModeFontColor, for: .normal)
+        adminsButton.layer.backgroundColor = UIColor.clear.cgColor
+        adminsButton.isExclusiveTouch = true
+        adminsButton.clipsToBounds = true
+        adminsButton.titleLabel?.font = getCustomFont(name: .Ubuntu_Light, size: 16)
+        adminsButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        adminsButton.addTarget(self, action: #selector(adminsButtonPressed), for: .touchDown)
+    }
+    
+    /** Objc methods for navigating between the different views in the paged horizontal scrollview*/
+    @objc func databaseButtonPressed(sender: UIButton){
+        currentPage = buttonBar!.getIndexOf(this: sender)!
+        turnPage()
+    }
+    
+    @objc func coreDataButtonPressed(sender: UIButton){
+        currentPage = buttonBar!.getIndexOf(this: sender)!
+        turnPage()
+    }
+    
+    @objc func metricsButtonPressed(sender: UIButton){
+        currentPage = buttonBar!.getIndexOf(this: sender)!
+        turnPage()
+    }
+    
+    @objc func adminsButtonPressed(sender: UIButton){
+        currentPage = buttonBar!.getIndexOf(this: sender)!
+        turnPage()
+    }
+    /** Objc methods for navigating between the different views in the paged horizontal scrollview*/
+    
+    func constructUI(){
+        navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 200))
+        navItem.rightBarButtonItem = closeButton
+        navItem.largeTitleDisplayMode = .always
+        navBar.prefersLargeTitles = true
+        navBar.setItems([navItem], animated: false)
+        
+        var barBgColor = appThemeColor
+        switch darkMode{
+        case true:
+            barBgColor = UIColor.black
+        case false:
+            barBgColor =  appThemeColor
+        }
+        buttonBar = underlinedButtonBar(buttons: [metricsButton, adminsButton], width: self.view.frame.width, height: 45, underlineColor: appThemeColor, underlineTrackColor: barBgColor.withAlphaComponent(0.8), underlineHeight: 2, backgroundColor: barBgColor, animated: true)
+        
+        view.addSubview(buttonBar!)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){[self] in
+            navBar.sizeToFit()
+            //Add the button bar below the navigation bar
+            buttonBar?.frame.origin = CGPoint(x: 0, y: navBar.frame.maxY)
+            
+            scrollView.frame = CGRect(x: 0, y: buttonBar!.frame.maxY, width: view.frame.width, height: view.frame.height)
+            stackView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            
+            scrollView.contentSize = CGSize(width: view.frame.width, height: scrollView.frame.height)
+            
+            let leading = NSLayoutConstraint(item: stackView, attribute: .leading, relatedBy: .equal, toItem: scrollView, attribute: .leading, multiplier: 1.0, constant: 0)
+            scrollView.addConstraint(leading)
+            let trailing = NSLayoutConstraint(item: stackView, attribute: .trailing, relatedBy: .equal, toItem: scrollView, attribute: .trailing, multiplier: 1.0, constant: 0)
+            scrollView.addConstraint(trailing)
+            let top = NSLayoutConstraint(item: stackView, attribute: .top, relatedBy: .equal, toItem: scrollView, attribute: .top, multiplier: 1.0, constant: 0)
+            scrollView.addConstraint(top)
+            let bottom = NSLayoutConstraint(item: stackView, attribute: .bottom, relatedBy: .equal, toItem: scrollView, attribute: .bottom, multiplier: 1.0, constant: 0)
+            scrollView.addConstraint(bottom)
+            let equalHeight = NSLayoutConstraint(item: stackView, attribute: .height, relatedBy: .equal, toItem: scrollView, attribute: .height, multiplier: 1.0, constant: 0)
+            scrollView.addConstraint(equalHeight)
+            leading.isActive = true
+            trailing.isActive = true
+            top.isActive = true
+            bottom.isActive = true
+            equalHeight.isActive = true
+            /**Set priority of this constraint to 1 to enable stack view and stack view subview user interactions, if this isn't set then taps aren't sent to the views as a result of constraint hiccups*/
+            equalHeight.priority = UILayoutPriority(1000)
+        }
+        
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        /**Scrollview can override touches to the content within an use those touches as parameters for movement*/
+        scrollView.canCancelContentTouches = true
+        scrollView.isPagingEnabled = true
+        scrollView.clipsToBounds = false
+        scrollView.delaysContentTouches = true
+        scrollView.isExclusiveTouch = true
+        
+        stackView.backgroundColor = UIColor.clear
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 0
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.isUserInteractionEnabled = true
+        stackView.semanticContentAttribute = .forceLeftToRight
+        
+        stackView.addArrangedSubview(databaseView)
+        stackView.addArrangedSubview(coreDataView)
+        stackView.addArrangedSubview(metricsView)
+        stackView.addArrangedSubview(adminsView)
+        
+        scrollView.addSubview(stackView)
+        self.view.addSubview(scrollView)
+        self.view.addSubview(navBar)
+    }
+
+    /** ScrollView Button Bar Methods*/
     /**Override default listener for UIScrollview to inform the instantiated page control of a page change*/
     public override func scrollViewDidScroll(_ UIScrollView: UIScrollView){
         /** Prevent the smaller scrollviews that scroll in the same direction as the parent scroll view from affecting the parent scrollview's content offset*/
@@ -80,25 +247,3 @@
         turnPage()
     }
     /** ScrollView Button Bar Methods*/
-
-/** Objc methods for navigating between the different views in the paged horizontal scrollview*/
-    @objc func databaseButtonPressed(sender: UIButton){
-        currentPage = buttonBar!.getIndexOf(this: sender)!
-        turnPage()
-    }
-    
-    @objc func coreDataButtonPressed(sender: UIButton){
-        currentPage = buttonBar!.getIndexOf(this: sender)!
-        turnPage()
-    }
-    
-    @objc func metricsButtonPressed(sender: UIButton){
-        currentPage = buttonBar!.getIndexOf(this: sender)!
-        turnPage()
-    }
-    
-    @objc func adminsButtonPressed(sender: UIButton){
-        currentPage = buttonBar!.getIndexOf(this: sender)!
-        turnPage()
-    }
-    /** Objc methods for navigating between the different views in the paged horizontal scrollview*/
